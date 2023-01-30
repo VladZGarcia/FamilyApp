399535500a29d0d3b8f04fa185e93cf29a62e63c
VladZGarcia <vlad.garcia@gmail.com>
2023-01-30 11:28:30 +0100

    Initial Commit

---
+++ FamilyApp/FamilyAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  FamilyAppApp.swift
+//  FamilyApp
+//
+//  Created by Hugo Garcia on 2023-01-30.
+//
+
+import SwiftUI
+
+@main
+struct FamilyAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
